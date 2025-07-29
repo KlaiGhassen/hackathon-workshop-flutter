550a1a9ad2cf536de0fa3ed6c1756cb6dc8590aa
KlaiGhassen <ghassen.klai@esprit.tn>
2025-07-29 13:36:29 +0100

    5th commit

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  
+  // Enable CORS
+  app.enableCors({
+    origin: true, // Allow all origins in development
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+  
   app.useGlobalPipes(new ValidationPipe());
   app.useStaticAssets(join(__dirname, '..', 'uploads'), {
     prefix: '/uploads/',
