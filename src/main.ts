e69f781c5db3e6bd675331bed48af615746351e6
KlaiGhassen <ghassen.klai@esprit.tn>
2025-07-29 14:34:08 +0100

    test

--- src/main.ts
+++ src/main.ts
@@ -35,7 +35,7 @@ async function bootstrap() {
     .build();
   
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('/api', app, document, {
+  SwaggerModule.setup('', app, document, {
     swaggerOptions: {
       persistAuthorization: false,
     },
